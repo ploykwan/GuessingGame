bf7d60c87651a51bddf972844014d13a0267cfbc
Kwankaew Uttama <kwankaew.u@ku.th>
2017-01-20 18:20:52 +0700

    add soruce code

---
+++ GameConsole.java
@@ -0,0 +1,25 @@
+package oop2;
+import java.util.Scanner;
+
+public class GameConsole{
+	
+	/**
+	 * The play method plays a game using input from a user.
+	 * @param game was use by the class game
+	 * @return the lastest number that user guess(the correct number)
+	 */
+	public int play(Game game){
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Guessing Game");
+		System.out.println("Your guess?");
+		int num , cnt = 0;
+		do{
+		System.out.println(game.getHint());
+		num = scan.nextInt();
+		}while(!game.guess(num));
+		System.out.println(game.getHint());
+		System.out.println("You used "+game.getCount()+" guesses.");
+		return num;
+	}
+}
+
---
+++ Main.java
@@ -0,0 +1,13 @@
+package oop2;
+
+public class Main {
+	
+	/**
+	 * create objects and start the game
+	 */
+	public static void main(String[] args){
+		Game game = new Game(20);
+		GameConsole ui = new GameConsole();
+		ui.play(game);
+	}
+}
